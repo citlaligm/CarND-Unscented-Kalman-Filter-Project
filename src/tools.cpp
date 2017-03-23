354b0e1063e64f7bab90c5e5a6e46fb53d3d736e
citlaligm <gmtlali88@gmail.com>
2017-03-23 13:34:03 -0700

    Modifies how to calculate RMS in order to include rms for vx and vy

--- src/tools.cpp
+++ src/tools.cpp
@@ -7,8 +7,10 @@ Tools::~Tools() {}
 
 VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
                               const vector<VectorXd> &ground_truth) {
-    VectorXd rmse(5);
-	rmse << 0,0,0,0,0;
+    VectorXd rmse(4);
+    vector<VectorXd> estm;
+    vector<VectorXd> gt;
+	rmse << 0,0,0,0;
 
     // check the validity of the following inputs:
 	//  * the estimation vector size should not be zero
@@ -17,10 +19,32 @@ VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
     	cout<<"estimation vector size should not be zero OR estimation vector size should be equal to ground truth vector size"<<"\n";
     }
 
+    for (int i=0; i < estimations.size(); ++i) {
+      VectorXd converted(4);
+      converted << estimations[i][0],                           // px
+                   estimations[i][1],                           // py
+                   cos(float(estimations[i][3]))*estimations[i][2],    // vx
+                   sin(float(estimations[i][3]))*estimations[i][2];    // vy
+      estm.push_back(converted);
+    }
+
+
+    for (int i=0; i < ground_truth.size(); ++i) {
+          VectorXd converted(4);
+          converted << ground_truth[i][0],                           // px
+        		  ground_truth[i][1],                           // py
+                       cos(float(ground_truth[i][3]))*ground_truth[i][2],    // vx
+                       sin(float(ground_truth[i][3]))*ground_truth[i][2];    // vy
+          gt.push_back(converted);
+        }
+
+
 
 	//accumulate squared residuals
-	for(int i=0; i < estimations.size(); ++i){
-        VectorXd residual=estimations[i]-ground_truth[i];
+	for(int i=0; i < estm.size(); ++i){
+
+        VectorXd residual=estm[i]-gt[i];
+
         residual = residual.array()*residual.array();
 		rmse += residual;
 		//cout<<"RMSE: "<<rmse<<"\n";
@@ -28,7 +52,7 @@ VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
 	}
 
 	//calculate the mean
-	int n = estimations.size();
+	int n = estm.size();
 	rmse = rmse/n;
 
 	//calculate the squared root
